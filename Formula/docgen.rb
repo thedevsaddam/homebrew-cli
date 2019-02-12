7c6113013da68152520d7329212acebd2f97065d
Saddam H <thedevsaddam@gmail.com>
2019-02-12 11:34:40 +0600

    Update docgen to v2.1

--- Formula/docgen.rb
+++ Formula/docgen.rb
@@ -1,9 +1,9 @@
 class Docgen < Formula
   desc "Docgen command line tool to generate API HTML/Markdown documentation from Postman collection."
   homepage "https://github.com/thedevsaddam/docgen"
-  url "https://github.com/thedevsaddam/docgen-bin/raw/master/v2/mac_amd64.zip"
-  version "2.0"
-  sha256 "7e1ae8b5da780ed28f4265f051526b73aaea69468322ab31cbe8ed6d1126a52d"
+  url "https://github.com/thedevsaddam/docgen-bin/raw/master/v2.1/mac_amd64.zip"
+  version "2.1"
+  sha256 "44932e58df46fabd19647d16a3da5136134928b90a9f85deb07aec9c1a843d8e"
 
   def install
     bin.install "docgen"
