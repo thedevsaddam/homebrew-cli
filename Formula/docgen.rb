9517dafdc39509cd9341edbb541b8bedad082785
Saddam H <thedevsaddam@gmail.com>
2019-02-12 20:06:57 +0600

    Update sha256 hash

--- Formula/docgen.rb
+++ Formula/docgen.rb
@@ -3,7 +3,7 @@ class Docgen < Formula
   homepage "https://github.com/thedevsaddam/docgen"
   url "https://github.com/thedevsaddam/docgen-bin/raw/master/v2.1/mac_amd64.zip"
   version "2.1"
-  sha256 "44932e58df46fabd19647d16a3da5136134928b90a9f85deb07aec9c1a843d8e"
+  sha256 "7c48f70b9b71f674382e5c958f0bba55fbcb7daeafb1c796c0b49a9f4c938fd2"
 
   def install
     bin.install "docgen"
