cd681a090f3aca170176f6bccc0a2fe67e935fb4
Saddam H <thedevsaddam@gmail.com>
2019-05-21 21:57:06 +0600

    Update q.rb

--- Formula/q.rb
+++ Formula/q.rb
@@ -1,8 +1,8 @@
-class q < Formula
+class Q < Formula
   desc "A command-line tool to query json/xml/yml/csv document"
   homepage "https://github.com/thedevsaddam/q"
   url "https://github.com/thedevsaddam/q/raw/master/q"
-  version "1.0"
+  version "0.1"
   sha256 "0047c2fba21ba91532eafbc8f4fe8a3a64fbb844adb818c2a1ce2581ed7e817b"
 
   def install
