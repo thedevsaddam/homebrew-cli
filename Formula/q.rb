d64c3bef22583fd97e7c8b2b7c685816821f7b18
Saddam H <thedevsaddam@gmail.com>
2019-05-21 21:54:09 +0600

    Update q.rb

--- Formula/q.rb
+++ Formula/q.rb
@@ -1,4 +1,4 @@
-class qcli < Formula
+class q < Formula
   desc "A command-line tool to query json/xml/yml/csv document"
   homepage "https://github.com/thedevsaddam/q"
   url "https://github.com/thedevsaddam/q/raw/master/q"
@@ -6,6 +6,6 @@ class qcli < Formula
   sha256 "0047c2fba21ba91532eafbc8f4fe8a3a64fbb844adb818c2a1ce2581ed7e817b"
 
   def install
-    bin.install "qcli"
+    bin.install "q"
   end
 end
